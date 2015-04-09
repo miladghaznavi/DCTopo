676a85f383e444e1f420a400f67192efeacc4e49
miladghaznavi <eghaznavi@scsmac089.cs.uwaterloo.ca>
2015-04-09 15:04:57 -0400

    Initial Commit

---
+++ DCTopo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DCTopo
+//
+//  Created by Milad Ghaznavi on 4/9/15.
+//  Copyright (c) 2015 Milad Ghaznavi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
